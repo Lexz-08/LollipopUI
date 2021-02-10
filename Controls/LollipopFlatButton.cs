7212d8c58e1bb5b633eff126d031549ad6b48f69
sh4d0w4RCH3R415 <misterjeremiahjordan@live.com>
2021-02-09 20:49:55 -0800

    Add project files.

---
+++ Controls/LollipopFlatButton.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class LollipopFlatButton : Control
+{
+
+    #region  Variables
+
+    Timer AnimationTimer = new Timer { Interval = 1 };
+
+    FontManager font = new FontManager();
+    StringFormat SF = new StringFormat();
+    Rectangle R;
+
+    bool Focus = false;
+
+    int xx;
+    int yy;
+
+    float SizeAnimation = 0;
+    float SizeIncNum;
+
+    string fontcolor = "#508ef5";
+
+    Color EnabledBGColor;
+    Color EnabledBorderColor;
+    Color StringColor;
+
+    Color DisabledStringColor = ColorTranslator.FromHtml("#969aa0");
+
+    #endregion
+    #region  Properties
+
+    [Category("Appearance")]
+    public string FontColor
+    {
+        get { return fontcolor; }
+        set
+        {
+            fontcolor = value;
+            Invalidate();
+        }
+    }
+
+    [Browsable(false)]
+    public override Font Font
+    {
+        get { return base.Font; }
+        set { base.Font = value; }
+    }
+
+    [Browsable(false)]
+    public override Color ForeColor
+    {
+        get { return base.ForeColor; }
+        set { base.ForeColor = value; }
+    }
+
+    #endregion
+    #region  Events
+
+    protected override void OnMouseEnter(EventArgs e)
+    {
+        base.OnMouseEnter(e);
+
+        EnabledBGColor = Color.FromArgb(30, StringColor);
+        EnabledBorderColor = Color.FromArgb(20, StringColor);
+        Refresh();
+    }
+    protected override void OnMouseLeave(EventArgs e)
+    {
+        base.OnMouseLeave(e);
+
+        EnabledBGColor = Parent.BackColor;
+        EnabledBorderColor = Parent.BackColor;
+        Refresh();
+    }
+    protected override void OnMouseDown(MouseEventArgs e)
+    {
+        base.OnMouseDown(e);
+
+        EnabledBGColor = Color.FromArgb(30, StringColor);
+        Refresh();
+
+        xx = e.X;
+        yy = e.Y;
+
+        Focus = true;
+        AnimationTimer.Start();
+        Invalidate();
+    }
+    protected override void OnMouseUp(MouseEventArgs e)
+    {
+        base.OnMouseUp(e);
+        Focus = false;
+        AnimationTimer.Start();
+        Invalidate();
+    }
+
+    protected override void OnTextChanged(System.EventArgs e)
+    {
+        base.OnTextChanged(e);
+        Invalidate();
+    }
+    protected override void OnSizeChanged(EventArgs e)
+    {
+        base.OnSizeChanged(e);
+        R = new Rectangle(0, 1, Width, Height);
+    }
+
+    #endregion
+
+    public LollipopFlatButton()
+    {
+        SetStyle((ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.SupportsTransparentBackColor | ControlStyles.UserPaint), true);
+        DoubleBuffered = true;
+
+        Size = new Size(143, 41);
+        BackColor = Color.Transparent;
+
+        SF.Alignment = StringAlignment.Center;
+        SF.LineAlignment = StringAlignment.Center;
+
+        AnimationTimer.Tick += new EventHandler(AnimationTick);
+    }
+
+    protected override void OnResize(System.EventArgs e)
+    {
+        base.OnResize(e);
+        SizeIncNum = 10;
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        var G = e.Graphics;
+        G.SmoothingMode = SmoothingMode.HighQuality;
+        G.Clear(Parent.BackColor);
+
+        StringColor = ColorTranslator.FromHtml(fontcolor);
+
+        var BG = DrawHelper.CreateRoundRect(1, 1, Width - 3, Height - 3, 1);
+        Region region = new Region(BG);
+
+        G.FillPath(new SolidBrush(Enabled ? EnabledBGColor : Parent.BackColor), BG);
+        G.DrawPath(new Pen(Enabled ? EnabledBorderColor : Parent.BackColor), BG);
+
+        G.SetClip(region, CombineMode.Replace);
+
+        //The Ripple Effect
+        G.FillEllipse(new SolidBrush(Color.FromArgb(30, StringColor)), xx - (SizeAnimation / 2), yy - (SizeAnimation / 2), SizeAnimation, SizeAnimation);
+        
+        G.DrawString(Text, font.Roboto_Medium10, new SolidBrush(Enabled? StringColor :DisabledStringColor), R, SF);
+    }
+
+    protected void AnimationTick(object sender, EventArgs e)
+    {
+        if (Focus)
+        {
+            if (SizeAnimation < Width + 250)
+            {
+                SizeAnimation += SizeIncNum;
+                this.Invalidate();
+            }
+        }
+        else
+        {
+            if (SizeAnimation > 0)
+            {
+                SizeAnimation = 0;
+                this.Invalidate();
+            }
+        }
+    }
+}
---
+++ Controls/LollipopTabControl.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+public class LollipopTabControl : TabControl
+{
+	FontManager fontManager = new FontManager();
+
+	public event EventHandler TabColorChanged;
+	protected virtual void OnTabColorChanged(EventArgs e)
+	{
+		TabColorChanged?.Invoke(this, e);
+		Invalidate();
+	}
+
+	private Color tabColor = Color.FromArgb(51, 182, 121);
+	
+	[Category("Appearance")]
+	public Color TabColor
+	{
+		get { return tabColor; }
+		set { tabColor = value; OnTabColorChanged(null); }
+	}
+
+	public override Rectangle DisplayRectangle
+	{
+		get
+		{
+			Rectangle rect = base.DisplayRectangle;
+			return new Rectangle(rect.Left - 4, rect.Top - 4, rect.Width + 8, rect.Height + 8);
+		}
+	}
+	protected override CreateParams CreateParams
+	{
+		get
+		{
+			CreateParams cp = base.CreateParams;
+			cp.ExStyle |= 0x02000000;
+			return cp;
+		}
+	}
+
+	protected override void OnPaint(PaintEventArgs e)
+	{
+		base.OnPaint(e);
+		var gfx = e.Graphics;
+		gfx.SmoothingMode = SmoothingMode.HighQuality;
+		gfx.PixelOffsetMode = PixelOffsetMode.HighQuality;
+		gfx.TextRenderingHint = TextRenderingHint.ClearTypeGridFit;
+		gfx.Clear(Parent.BackColor);
+
+		for (int i = 0; i < TabCount; i++)
+		{
+			if (i == SelectedIndex)
+			{
+				gfx.FillRectangle(new SolidBrush(tabColor), GetTabRect(i).X + 3, ItemSize.Height - 3, ItemSize.Width - 6, 3);
+				gfx.DrawString(TabPages[i].Text, fontManager.Roboto_Medium10, new SolidBrush(tabColor), GetTabRect(i),
+					new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+			}
+			else
+			{
+				gfx.DrawString(TabPages[i].Text, fontManager.Roboto_Medium10, new SolidBrush(Color.Black), GetTabRect(i),
+					new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+			}
+		}
+	}
+
+	public LollipopTabControl()
+	{
+		SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.UserPaint, true);
+		DoubleBuffered = true;
+		SizeMode = TabSizeMode.Fixed;
+		ItemSize = new Size(120, 40);
+	}
+}
